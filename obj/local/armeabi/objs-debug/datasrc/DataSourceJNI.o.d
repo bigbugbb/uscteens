e6880c7ffedb505986dbb462d20fd413625b772c
BinBo <bigbug@BinBos-MacBook-Pro.local>
2013-03-05 09:21:38 -0500

    +++

---
+++ obj/local/armeabi/objs-debug/datasrc/DataSourceJNI.o.d
@@ -0,0 +1,41 @@
+/Develop/Git/USC_Teen/obj/local/armeabi/objs-debug/datasrc/DataSourceJNI.o: \
+ /Develop/Git/USC_Teen/jni/datasrc/DataSourceJNI.cpp \
+ /Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/jni.h \
+ /Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/assert.h \
+ /Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /Develop/Git/USC_Teen/jni/datasrc/DataSource.h \
+ /Develop/Git/USC_Teen/jni/datasrc/BaseClasses.h \
+ /Develop/Git/USC_Teen/jni/datasrc/Config.h \
+ /Develop/Android/android-ndk-r8d/sources/cxx-stl/gnu-libstdc++/4.6/include/cassert \
+ /Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/android/log.h \
+ /Develop/Android/android-ndk-r8d/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Develop/Android/android-ndk-r8d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ /Develop/Android/android-ndk-r8d/sources/cxx-stl/gnu-libstdc++/4.6/include/string
+
+/Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/assert.h:
+
+/Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/Develop/Git/USC_Teen/jni/datasrc/DataSource.h:
+
+/Develop/Git/USC_Teen/jni/datasrc/BaseClasses.h:
+
+/Develop/Git/USC_Teen/jni/datasrc/Config.h:
+
+/Develop/Android/android-ndk-r8d/sources/cxx-stl/gnu-libstdc++/4.6/include/cassert:
+
+/Develop/Android/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+/Develop/Android/android-ndk-r8d/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Develop/Android/android-ndk-r8d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+/Develop/Android/android-ndk-r8d/sources/cxx-stl/gnu-libstdc++/4.6/include/string:
