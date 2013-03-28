2430395087d999f636684659d17830eff98efadf
BinBo <bigbug@BinBos-MacBook-Pro.local>
2013-03-27 23:22:42 -0400

    add the reward view for displaying html

---
+++ src/edu/neu/android/mhealth/uscteensver1/views/RewardView.java
@@ -0,0 +1,16 @@
+package edu.neu.android.mhealth.uscteensver1.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.webkit.WebView;
+
+public class RewardView extends WebView {
+
+	public RewardView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+		setVisibility(View.GONE);
+	}
+
+}
