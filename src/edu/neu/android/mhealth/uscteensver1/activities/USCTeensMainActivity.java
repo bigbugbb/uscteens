8a4bfff49bcba512052d3ac0f4c64a7db5a60304
BinBo <bigbug@BinBos-MacBook-Pro.local>
2013-06-13 12:32:39 -0400

    +

--- src/edu/neu/android/mhealth/uscteensver1/activities/USCTeensMainActivity.java
+++ src/edu/neu/android/mhealth/uscteensver1/activities/USCTeensMainActivity.java
@@ -130,8 +130,11 @@ public class USCTeensMainActivity extends USCTeensBaseActivity implements OnTouc
 		USCTeensGlobals.sUpdateConfig = !newVersion.equals(oldVersion);
 		if (USCTeensGlobals.sUpdateConfig) {
 			String dirPath = Globals.EXTERNAL_DIRECTORY_PATH + File.separator +
-					Globals.DATA_DIRECTORY + USCTeensGlobals.ANNOTATION_FOLDER;
+					Globals.DATA_DIRECTORY + USCTeensGlobals.ACTIVITY_FOLDER;
 			FileHelper.deleteDir(dirPath);
+			dirPath = Globals.EXTERNAL_DIRECTORY_PATH + File.separator +
+					Globals.DATA_DIRECTORY + USCTeensGlobals.ANNOTATION_FOLDER;
+			FileHelper.deleteDir(dirPath);			
 		}
 	}
 
