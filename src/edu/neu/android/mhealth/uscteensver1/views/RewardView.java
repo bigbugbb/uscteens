c585cb0fd86eaa562d0034878a8cee92b0c88dd9
BinBo <bigbug@BinBos-MacBook-Pro.local>
2013-07-22 18:00:02 -0400

    get rid of many html files

--- src/edu/neu/android/mhealth/uscteensver1/views/RewardView.java
+++ src/edu/neu/android/mhealth/uscteensver1/views/RewardView.java
@@ -1,10 +1,18 @@
 package edu.neu.android.mhealth.uscteensver1.views;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebView;
+import edu.neu.android.mhealth.uscteensver1.TeensGlobals;
+import edu.neu.android.wocketslib.support.DataStorage;
+import edu.neu.android.wocketslib.utils.DateHelper;
 
 public class RewardView extends WebView {
 	protected final static String DEFAULT_URL = "file:///android_asset/rewards/default.html";
@@ -13,9 +21,9 @@ public class RewardView extends WebView {
 		super(context, attrs);
 		setVisibility(View.GONE);
 		
-        getSettings().setJavaScriptEnabled(true);
-        setScrollBarStyle(0);
-
+        JavaScriptInterface jsInterface = new JavaScriptInterface();
+		getSettings().setJavaScriptEnabled(true);
+		addJavascriptInterface(jsInterface, "JSInterface");
        	loadUrl(DEFAULT_URL);        
 	}
 
@@ -23,5 +31,52 @@ public class RewardView extends WebView {
 	public boolean onTouchEvent(MotionEvent event) {
 		// TODO Auto-generated method stub
 		return super.onTouchEvent(event);
+	}
+	
+	public class JavaScriptInterface {
+
+	    public JavaScriptInterface() {
+	    }
+	    
+	    public String getCountOfDayFromStartDate() {
+	    	String startDate = DataStorage.getStartDate(getContext(), "");
+	    	String selectedDate = DataStorage.GetValueString(getContext(), TeensGlobals.CURRENT_SELECTED_DATE, "");
+	    	
+	    	for (int i = 1; i <= 14; ++i) {
+	    		try {
+	    			Date start = DateHelper.serverDateFormat.parse(startDate);
+					Calendar c = Calendar.getInstance();   			  
+				    c.setTime(start);   
+				    c.add(Calendar.DATE, i - 1);   
+				    // compare the date 
+				    if (DateHelper.serverDateFormat.format(c.getTime()).equals(selectedDate)) {
+				    	return i + "";
+				    }
+	    		} catch (ParseException e) {
+	    			e.printStackTrace();
+	    		}
+	    	}
+	    	
+	    	return "unknown date";
+	    }
+	    
+	    public String getDate(int id) {
+	    	String result;
+	    	String startDate = DataStorage.getStartDate(getContext(), "");
+	    	
+		    try {
+				Date start = DateHelper.serverDateFormat.parse(startDate);
+				Calendar c = Calendar.getInstance();   			  
+			    c.setTime(start);   
+			    c.add(Calendar.DATE, id - 1);   
+			    Date d = c.getTime();   
+			    result = DateHelper.serverDateFormat.format(d); 
+			} catch (ParseException e) {
+				e.printStackTrace();
+				result = "unknown date";
+			}
+		    
+		    return result;
+	    }
 	}
 }
