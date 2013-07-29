5a55f913d0f7b41d7cb9ff2ebe112a0b6908ba45
BinBo <bigbug@BinBos-MacBook-Pro.local>
2013-07-29 11:43:38 -0400

    Add TeensSurveyTestActivity to separate the test mode and the real mode.

---
+++ src/edu/neu/android/mhealth/uscteensver1/activities/TeensSurveyTestActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.android.mhealth.uscteensver1.activities;
+
+/**
+ * <p>
+ * This class is used to avoid the possible conflicts between the test mode and
+ * real mode of the TeensSurveyActivity. So when it's in the test mode, this class
+ * is used instead of the TeensSurveyActivity.
+ * </p>
+ * @author bigbug
+ *
+ */
+public class TeensSurveyTestActivity extends TeensSurveyActivity {
+	
+}
