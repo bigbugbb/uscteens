35d72bfcedb4e6f0a23410505c37b51248b69913
BinBo <bigbug@BinBos-MacBook-Pro.local>
2013-07-30 18:53:23 -0400

    Fixed a bug that leads to crash when parse the date time in AccelDataChecker by checking if the current file is a file or a directory

--- src/edu/neu/android/mhealth/uscteensver1/data/AccelDataChecker.java
+++ src/edu/neu/android/mhealth/uscteensver1/data/AccelDataChecker.java
@@ -1,6 +1,7 @@
 package edu.neu.android.mhealth.uscteensver1.data;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -65,21 +66,26 @@ public class AccelDataChecker {
 		AccelDataWrap accelDataWrap = new AccelDataWrap();
 		
 		for (int i = 0; i < hourDirs.length; ++i) {
-			int targetHour = Integer.valueOf(hourDirs[i].substring(hourDirs[i].lastIndexOf('/') + 1));
+			File file = new File(hourDirs[i]);
+			if (file.isFile()) { continue; }
+			String hour = hourDirs[i].substring(hourDirs[i].lastIndexOf('/') + 1);
+			int targetHour = Integer.valueOf(hour);
 			if (targetHour < hourFrom || targetHour > hourTo) { // 12AM can not be handled here!!!
 				continue;
 			}
 			
 			try {
 				// each hour corresponds to one .bin file
-				String[] filePaths = FileHelper.getFilePathsDir(hourDirs[i]);
-				String filePath = filePaths[0];			
-				for (String path : filePaths) {				
-					String extName = path.substring(path.lastIndexOf("."), path.length());
-					if (extName.equals(".bin")) {
-						filePath = path;
+				String[] fileNames = new File(hourDirs[i]).list(new FilenameFilter() {
+					@Override
+					public boolean accept(File dir, String filename) {
+						return filename.endsWith(".csv");
 					}
+				});
+				if (fileNames == null || fileNames.length == 0) {
+					continue;
 				}
+				String filePath = hourDirs[i] + File.separator + fileNames[0];
 				// load the hourly data from .bin file
 				ArrayList<AccelData> hourlyAccelData = new ArrayList<AccelData>();						
 				DataSource.loadHourlyRawAccelData(filePath, hourlyAccelData, false);
