539c2fb5e35e8bbde509239837a14989cce62e7d
BinBo <bigbug@BinBos-MacBook-Pro.local>
2013-08-19 10:18:33 -0400

    Changed all the methods to use addLabel with sending a date rather than a string of a date.

--- src/edu/neu/android/mhealth/uscteensver1/TeensBroadcastReceiverProcessor.java
+++ src/edu/neu/android/mhealth/uscteensver1/TeensBroadcastReceiverProcessor.java
@@ -1,98 +1,90 @@
 package edu.neu.android.mhealth.uscteensver1;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import android.annotation.SuppressLint;
 import edu.neu.android.mhealth.uscteensver1.data.Labeler;
 import edu.neu.android.wocketslib.broadcastreceivers.BroadcastReceiverProcessor;
 import edu.neu.android.wocketslib.utils.Log;
 
 public class TeensBroadcastReceiverProcessor extends BroadcastReceiverProcessor {
 	private static final String TAG = "TeensBroadcastReceiverProcessor";
-
-	@SuppressLint("SimpleDateFormat")
-	private String getDateString() {
-		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()); 
-	}
 	
-	//TODO change all the methods below to use addLabel with sending a date (vs a string of a date). Will clean up code. 
 	@Override
 	public void respondSendSMS() {		
 		Log.d(TAG, "Respond SendSMS in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Sent SMS"); 		
+		Labeler.getInstance().addLabel(new Date(), "Sent SMS"); 		
 	}
 	
 	@Override
 	public void respondScreenOn() {		
 		Log.d(TAG, "Respond Screen ON in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Screen on"); 		
+		Labeler.getInstance().addLabel(new Date(), "Screen on"); 		
 	}
 	
 	@Override
 	public void respondPhoneBooted() {
 		Log.d(TAG, "Respond Phone Booted in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Restart phone"); 		
+		Labeler.getInstance().addLabel(new Date(), "Restart phone"); 		
 	}
 	
 	@Override
 	public void respondEndCall() {
 		Log.d(TAG, "Respond End Call in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "End call"); 		
+		Labeler.getInstance().addLabel(new Date(), "End call"); 		
 	}
 		
 	@Override
 	public void respondStartCall() {
 		Log.d(TAG, "Respond Start Call in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Start call"); 		
+		Labeler.getInstance().addLabel(new Date(), "Start call"); 		
 	}
 
 	@Override
 	public void respondCallOut() {
 		Log.d(TAG, "Respond Call Out in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Call out"); 		
+		Labeler.getInstance().addLabel(new Date(), "Call out"); 		
 	}
 
 	@Override
 	public void respondAirplaneMode() {
 		Log.d(TAG, "Respond Airplane Mode in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Plane mode"); 		
+		Labeler.getInstance().addLabel(new Date(), "Plane mode"); 		
 	}
 
 	@Override
 	public void respondCallIn() {
 		Log.d(TAG, "Respond Call In in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Receive call"); 		
+		Labeler.getInstance().addLabel(new Date(), "Receive call"); 		
 	}
 
 	@Override
 	public void respondPowerConnected() {
 		Log.d(TAG, "Respond Power Connected in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Charge phone"); 		
+		Labeler.getInstance().addLabel(new Date(), "Charge phone"); 		
 	}
 
 	@Override
 	public void respondPowerDisconnected() {
 		Log.d(TAG, "Respond Power Disconnected in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Stop charging"); 		
+		Labeler.getInstance().addLabel(new Date(), "Stop charging"); 		
 	}
 
 	@Override
 	public void respondSMSReceived() {
 		Log.d(TAG, "Respond Received SMS in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Received SMS"); 		
+		Labeler.getInstance().addLabel(new Date(), "Received SMS"); 		
 	}
 	
 	@Override
 	public void respondLocationChanged() {
 		Log.d(TAG, "Respond Location Changed in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Location changed"); 		
+		Labeler.getInstance().addLabel(new Date(), "Location changed"); 		
 	}
 
 	@Override
 	public void respondHeadsetPluggedIn() {
 		Log.d(TAG, "Respond Headset Plugged In in " + TAG); 
-		Labeler.getInstance().addLabel(getDateString(), "Plug earphones"); 		
+		Labeler.getInstance().addLabel(new Date(), "Plug earphones"); 		
 	}
 
 }
